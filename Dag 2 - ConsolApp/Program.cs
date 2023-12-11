e7592b85b32d9dd64a719431691c6dc6116d68d5
Adem Akat <adak0001@edu.mercantec.dk>
2023-12-11 15:13:24 +0100

    update

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -150,14 +150,6 @@ if (nivis.Contains("cole"))
 
 //Because the message.Contains("fox") returns a true or false value, it qualifies as a Boolean expression and can be used in an if statement.
 
-
-
-
-
-//
-
-Console.WriteLine();
-
 Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
@@ -170,28 +162,38 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
 }
 
-if ((roll1 == roll2) && (roll2 == roll3))
+if (total >= 16)
 {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    Console.WriteLine("You win a new car!");
 }
-
-if (total >= 15)
+else if (total >= 10)
 {
-    Console.WriteLine("You win!");
+    Console.WriteLine("You win a new laptop!");
 }
-
-if (total < 15)
+else if (total == 7)
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
 
 /*
 Recap
+
 Use an if statement to branch your code logic. The if decision statement will execute code in its code block if its Boolean expression equates to true. 
 Otherwise, the runtime will skip over the code block and continue to the next line of code after the code block.
 A Boolean expression is any expression that returns a Boolean value.
@@ -199,5 +201,37 @@ Boolean operators will compare the two values on its left and right for equality
 A code block is defined by curly braces { }. It collects lines of code that should be treated as a single unit.
 The logical AND operator && aggregates two expressions so that both subexpressions must be true in order for the entire expression to be true.
 The logical OR operator || aggregates two expressions so that if either subexpression is true, the entire expression is true.
+
+The combination of if and else statements allows you to test for one condition, and then perform one of two outcomes. The code block for the if will be run when the Boolean expression is true, and the code block for the else will be run when the Boolean expression is false.
+You can nest if statements to narrow down a possible condition. However, you should consider using the if, else if, and else statements instead.
+Use else if statements to create multiple exclusive conditions.
+An else is optional, but it must always come last when included.
 */
 
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
