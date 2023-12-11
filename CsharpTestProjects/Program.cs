338afd8382e810713e2639e345967b8e4180789a
Adem Akat <adak0001@edu.mercantec.dk>
2023-12-11 13:40:20 +0100

    update

--- CsharpTestProjects/Program.cs
+++ CsharpTestProjects/Program.cs
@@ -45,7 +45,7 @@ The third WriteLine() method uses a method signature that accepts a string param
 
 
 
-/*
+
 Random dice = new Random();
 int roll1 = dice.Next();
 int roll2 = dice.Next(101);
@@ -70,8 +70,6 @@ The second version of the Next() method specifies the maximum value as an upper 
 The third version of the Next() method specifies both the minimum and maximum values, so in this case, you can expect a random value between 50 and 100.
 
 */
-
-/*
 Console.WriteLine();
 
 
@@ -94,9 +92,7 @@ Console.WriteLine(largerValue);
 
 
 Console.WriteLine();
-*/
 
-/*
 // Add decision logic to your code using 'if', 'else', and 'else if' statements in C#
 
 Random hejmar = new Random();
@@ -149,55 +145,4 @@ if (nivis.Contains("cole"))
 }
 
 //Because the message.Contains("fox") returns a true or false value, it qualifies as a Boolean expression and can be used in an if statement.
-
-
-
-
-
-//
-
-Console.WriteLine();
-
-Random dice = new Random();
-
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
-}
-
-if ((roll1 == roll2) && (roll2 == roll3))
-{
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
-}
-
-if (total >= 15)
-{
-    Console.WriteLine("You win!");
-}
-
-if (total < 15)
-{
-    Console.WriteLine("Sorry, you lose.");
-}
-
-/*
-Recap
-Use an if statement to branch your code logic. The if decision statement will execute code in its code block if its Boolean expression equates to true. 
-Otherwise, the runtime will skip over the code block and continue to the next line of code after the code block.
-A Boolean expression is any expression that returns a Boolean value.
-Boolean operators will compare the two values on its left and right for equality, comparison, and more.
-A code block is defined by curly braces { }. It collects lines of code that should be treated as a single unit.
-The logical AND operator && aggregates two expressions so that both subexpressions must be true in order for the entire expression to be true.
-The logical OR operator || aggregates two expressions so that if either subexpression is true, the entire expression is true.
-*/
 
